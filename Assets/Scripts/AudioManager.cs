98afdeebd257c0f919589128a9c6cef375fd9186
LoganPriceCope <146819064+LoganPriceCope@users.noreply.github.com>
2024-03-25 09:03:39 +0000

    FinalProject

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
 
+    public static AudioManager instance;
+    public bool muted = false;
     [Header("--- Audio Source ---")]
     [SerializeField] AudioSource musicSource;
     [SerializeField] AudioSource SFXSource;
@@ -11,9 +13,23 @@ public class AudioManager : MonoBehaviour
     public AudioClip background;
     public AudioClip button;
 
-    private void Awake()
+
+    void Awake()
     {
-        GameObject.DontDestroyOnLoad(this);
+        if (instance == null)
+        {
+            // if instance is null, store a reference to this instance
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+            print("dont destroy");
+        }
+        else
+        {
+            // Another instance of this gameobject has been made so destroy it
+            // as we already have one
+            print("do destroy");
+            Destroy(gameObject);
+        }
     }
     private void Start()
     {
--- Assets/Scripts/VolumeSettings.cs
+++ Assets/Scripts/VolumeSettings.cs
@@ -8,6 +8,7 @@ public class VolumeSettings : MonoBehaviour
     [SerializeField] private AudioMixer myMixer;
     [SerializeField] private Slider musicSlider;
     [SerializeField] private Slider SFXSlider;
+    
 
     private void Start()
     {
@@ -20,26 +21,89 @@ public class VolumeSettings : MonoBehaviour
             SetMusicVolume();
             SetSFXVolume();
         }
-        
+
+        if (!PlayerPrefs.HasKey("muted"))
+        {
+
+            PlayerPrefs.SetInt("muted", 0);
+        }
+        LoadMasterVolume();
+
+
     }
+
+    private void Update()
+    {
+        float vol;
+        vol = musicSlider.value;
+        PlayerPrefs.SetFloat("musicVolume", vol);
+
+        if (AudioManager.instance.muted)
+           vol = 0.0001f;
+
+        print("mvol=" + vol);
+
+        myMixer.SetFloat("music", Mathf.Log10(vol) * 20 );
+        
+
+
+        //
+        //PlayerPrefs.SetFloat("SFXVolume", volume);
+        //myMixer.SetFloat("SFX", Mathf.Log10(SFXSlider.value) * 20);
+    }
+
+    public void ToggleMusic()
+    {
+        print("Saving master vol");
+        if (AudioManager.instance.muted == false)
+        {
+            AudioManager.instance.muted = true;
+        }
+        else
+        {
+            AudioManager.instance.muted = false;
+        }
+        SaveMasterVolume();
+    }
+
+
+
     public void SetMusicVolume()
     {
-        float volume = musicSlider.value;
-        myMixer.SetFloat("music", Mathf.Log10(volume)*20);
-        PlayerPrefs.SetFloat("musicVolume", volume);
     }
 
     public void SetSFXVolume()
     {
         float volume = SFXSlider.value;
-        myMixer.SetFloat("SFX", Mathf.Log10(volume) * 20);
-        PlayerPrefs.SetFloat("SFXVolume", volume);
+        
+        
+    }
+
+    private void SaveMasterVolume()
+    {
+        PlayerPrefs.SetInt("muted", AudioManager.instance.muted ? 1 : 0);
+    }
+
+    private void LoadMasterVolume()
+    {
+        if( PlayerPrefs.GetInt("muted") == 0 )
+        {
+            AudioManager.instance.muted = false;
+        }
+        else
+        {
+            AudioManager.instance.muted = true;
+        }
+        
+
     }
 
     private void LoadVolume()
     {
+        print("loading vol");
         musicSlider.value = PlayerPrefs.GetFloat("musicVolume");
         SFXSlider.value = PlayerPrefs.GetFloat("SFXVolume");
+        
 
         SetMusicVolume();
         SetSFXVolume();
